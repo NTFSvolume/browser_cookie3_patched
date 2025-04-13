f2a778f7b593768483c85bcf00c8af5130f76e4a
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-04-13 18:46:44 -0500

    tests: apply ruff fixes and type hints to tests

--- tests/utils/browser_paths.py
+++ tests/utils/browser_paths.py
@@ -1,122 +1,122 @@
 import os
-import sys
 
-from . import BrowserName, logger
+from browser_cookie3 import _CURRENT_OS
+
+from . import BrowserName as B
+from . import logger
 
 
-def get_username():
+def get_username() -> str:
     try:
         return os.getlogin()
     except OSError:
-        return os.environ.get('USERNAME') or os.environ.get('USER') or ''
+        return os.environ.get("USERNAME") or os.environ.get("USER") or ""
 
 
-BIN_LOCATIONS = {
-    BrowserName.CHROME: {
-        'linux': ['/usr/bin/google-chrome-stable'],
-        'windows': [
-            r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe',
-            r'C:\Program Files\Google\Chrome\Application\chrome.exe'
+_USERNAME = get_username()
+
+
+_BIN_LOCATIONS = {
+    B.CHROME: {
+        "linux": ["/usr/bin/google-chrome-stable"],
+        "windows": [
+            r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe",
+            r"C:\Program Files\Google\Chrome\Application\chrome.exe",
         ],
         # Not tested
-        'macos': ['/Applications/Google Chrome.app/Contents/MacOS/Google Chrome']
+        "macos": ["/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"],
     },
-    BrowserName.CHROMIUM: {
-        'linux': ['/usr/bin/chromium', '/usr/bin/chromium-browser'],
-        'windows': [
-            r'C:\Program Files (x86)\Chromium\Application\chrome.exe',
-            r'C:\Program Files\Chromium\Application\chrome.exe'
+    B.CHROMIUM: {
+        "linux": ["/usr/bin/chromium", "/usr/bin/chromium-browser"],
+        "windows": [
+            r"C:\Program Files (x86)\Chromium\Application\chrome.exe",
+            r"C:\Program Files\Chromium\Application\chrome.exe",
         ],
         # Not tested
-        'macos': ['/Applications/Chromium.app/Contents/MacOS/Chromium']
+        "macos": ["/Applications/Chromium.app/Contents/MacOS/Chromium"],
     },
-    BrowserName.BRAVE: {
-        'linux': ['/usr/bin/brave', '/usr/bin/brave-browser'],
-        'windows': [
-            r'C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe',
-            r'C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe',
-            fr'C:\Users\{get_username()}\AppData\Local\BraveSoftware\Brave-Browser\Application\brave.exe'
+    B.BRAVE: {
+        "linux": ["/usr/bin/brave", "/usr/bin/brave-browser"],
+        "windows": [
+            r"C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe",
+            r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe",
+            rf"C:\Users\{_USERNAME}\AppData\Local\BraveSoftware\Brave-Browser\Application\brave.exe",
         ],
         # Not tested
-        'macos': ['/Applications/Brave Browser.app/Contents/MacOS/Brave Browser']
+        "macos": ["/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"],
     },
-    BrowserName.EDGE: {
-        'linux': ['/usr/bin/microsoft-edge-stable'],
-        'windows': [
-            r'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe',
-            r'C:\Program Files\Microsoft\Edge\Application\msedge.exe'
+    B.EDGE: {
+        "linux": ["/usr/bin/microsoft-edge-stable"],
+        "windows": [
+            r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe",
+            r"C:\Program Files\Microsoft\Edge\Application\msedge.exe",
         ],
         # Not tested
-        'macos': ['/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge']
+        "macos": ["/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge"],
     },
-    BrowserName.FIREFOX: {
-        'linux': ['/usr/bin/firefox'],
-        'windows': [
-            r'C:\Program Files (x86)\Mozilla Firefox\firefox.exe',
-            r'C:\Program Files\Mozilla Firefox\firefox.exe'
+    B.FIREFOX: {
+        "linux": ["/usr/bin/firefox"],
+        "windows": [
+            r"C:\Program Files (x86)\Mozilla Firefox\firefox.exe",
+            r"C:\Program Files\Mozilla Firefox\firefox.exe",
         ],
         # Not tested
-        'macos': ['/Applications/Firefox.app/Contents/MacOS/firefox']
+        "macos": ["/Applications/Firefox.app/Contents/MacOS/firefox"],
     },
-    BrowserName.LIBREWOLF: {
-        'linux': ['/usr/bin/librewolf'],
-        'windows': [
-            r'C:\Program Files (x86)\LibreWolf\librewolf.exe',
-            r'C:\Program Files\LibreWolf\librewolf.exe',
-            fr'C:\Users\{get_username()}\AppData\Local\LibreWolf\librewolf.exe'
+    B.LIBREWOLF: {
+        "linux": ["/usr/bin/librewolf"],
+        "windows": [
+            r"C:\Program Files (x86)\LibreWolf\librewolf.exe",
+            r"C:\Program Files\LibreWolf\librewolf.exe",
+            rf"C:\Users\{_USERNAME}\AppData\Local\LibreWolf\librewolf.exe",
         ],
         # Not tested
-        'macos': ['/Applications/LibreWolf.app/Contents/MacOS/LibreWolf']
+        "macos": ["/Applications/LibreWolf.app/Contents/MacOS/LibreWolf"],
     },
-    BrowserName.OPERA: {
-        'linux': ['/usr/bin/opera'],
-        'windows': [
-            r'C:\Program Files (x86)\Opera\opera.exe',
-            r'C:\Program Files\Opera\opera.exe',
-            fr'C:\Users\{get_username()}\AppData\Local\Programs\Opera\opera.exe'
+    B.OPERA: {
+        "linux": ["/usr/bin/opera"],
+        "windows": [
+            r"C:\Program Files (x86)\Opera\opera.exe",
+            r"C:\Program Files\Opera\opera.exe",
+            rf"C:\Users\{_USERNAME}\AppData\Local\Programs\Opera\opera.exe",
         ],
-        'macos': ['/Applications/Opera.app/Contents/MacOS/Opera']  # Not tested
+        "macos": ["/Applications/Opera.app/Contents/MacOS/Opera"],  # Not tested
     },
-    BrowserName.OPERA_GX: {
-        'linux': [],
-        'windows': [
-            r'C:\Program Files (x86)\Opera GX\opera.exe',
-            r'C:\Program Files\Opera GX\opera.exe',
-            fr'C:\Users\{get_username()}\AppData\Local\Programs\Opera GX\opera.exe'
-        ],
-        # Not tested
-        'macos': ['/Applications/Opera GX.app/Contents/MacOS/Opera GX']
-    },
-    BrowserName.VIVALDI: {
-        'linux': ['/usr/bin/vivaldi-stable'],
-        'windows': [
-            r'C:\Program Files (x86)\Vivaldi\Application\vivaldi.exe',
-            r'C:\Program Files\Vivaldi\Application\vivaldi.exe',
-            fr'C:\Users\{get_username()}\AppData\Local\Vivaldi\Application\vivaldi.exe'
+    B.OPERA_GX: {
+        "linux": [],
+        "windows": [
+            r"C:\Program Files (x86)\Opera GX\opera.exe",
+            r"C:\Program Files\Opera GX\opera.exe",
+            rf"C:\Users\{_USERNAME}\AppData\Local\Programs\Opera GX\opera.exe",
         ],
         # Not tested
-        'macos': ['/Applications/Vivaldi.app/Contents/MacOS/Vivaldi']
-    }
+        "macos": ["/Applications/Opera GX.app/Contents/MacOS/Opera GX"],
+    },
+    B.VIVALDI: {
+        "linux": ["/usr/bin/vivaldi-stable"],
+        "windows": [
+            r"C:\Program Files (x86)\Vivaldi\Application\vivaldi.exe",
+            r"C:\Program Files\Vivaldi\Application\vivaldi.exe",
+            rf"C:\Users\{_USERNAME}\AppData\Local\Vivaldi\Application\vivaldi.exe",
+        ],
+        # Not tested
+        "macos": ["/Applications/Vivaldi.app/Contents/MacOS/Vivaldi"],
+    },
 }
 
 
 class BinaryLocation:
-    def __init__(self, raise_not_found=False):
+    def __init__(self, raise_not_found: bool = False):
         self.__raise_not_found = raise_not_found
-        if sys.platform == 'darwin':
-            self.__os = 'macos'
-        elif sys.platform.startswith('linux') or 'bsd' in sys.platform.lower():
-            self.__os = 'linux'
-        elif sys.platform == "win32":
-            self.__os = 'windows'
-        else:
-            raise ValueError('unsupported os')
+        if _CURRENT_OS == "unknown":
+            raise ValueError("unsupported os")
+        self.__os = _CURRENT_OS
 
-    def get(self, browser: str) -> str:
-        for i in BIN_LOCATIONS[browser][self.__os]:
-            if os.path.exists(i):
-                logger.info(f'found {browser} binary at: {i}')
-                return i
+    def get(self, browser: str) -> str:  # type: ignore
+        for path in _BIN_LOCATIONS[browser][self.__os]:
+            if os.path.exists(path):
+                logger.info(f"found {browser} binary at: {path}")
+                return path
         if self.__raise_not_found:
-            raise FileNotFoundError('browser not found')
-        logger.warning(f'could not find {browser} binary')
+            raise FileNotFoundError("browser not found")
+        logger.warning(f"could not find {browser} binary")
